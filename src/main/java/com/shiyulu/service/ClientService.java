273adcf1217cc2f2d8e2537cf5b87b038ac61d24
JianXi <3146624068@qq.com>
2024-04-19 15:39:32 +0800

    客户相关接口

---
+++ src/main/java/com/shiyulu/service/ClientService.java
@@ -0,0 +1,22 @@
+package com.shiyulu.service;
+
+import com.shiyulu.pojo.Client;
+import com.shiyulu.pojo.PageBean;
+import com.shiyulu.pojo.Vehicle;
+import com.shiyulu.pojo.VehicleFault;
+
+public interface ClientService {
+    //分页查询所有的客户信息
+    PageBean queryAllClientInfo(Integer page, Integer pageSize);
+    //分页查询所有的客户车辆信息
+    Client queryClientInfoById(Integer clientId);
+    PageBean queryAllVehicleInfo(Integer page, Integer pageSize);
+    //根据客户ID查询客户车辆信息,客户车可能有多辆
+    PageBean queryVehicleInfoByClientId(Integer page, Integer pageSize,Integer clientId);
+    //分页查询所有的客户维修车辆信息
+    PageBean queryAllVehicleFaultInfo(Integer page, Integer pageSize);
+    //根据维修订单号查询客户车辆信息
+    VehicleFault queryVehicleFaultInfoByVFId(Integer vehicleFaultId);
+    //根据客户ID分页查询客户的所有维修车辆信息
+    public PageBean queryVehicleFaultInfoByClientId(Integer page, Integer pageSize,Integer clientId);
+}
