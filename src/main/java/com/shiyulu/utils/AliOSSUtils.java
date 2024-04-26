c7da3898752360c993a2d19cbd089b845d8d6452
cecily991 <Cecilywang991@gmail.com>
2024-04-26 20:07:23 +0800

    暂存0426版本

--- src/main/java/com/shiyulu/utils/AliOSSUtils.java
+++ src/main/java/com/shiyulu/utils/AliOSSUtils.java
@@ -16,9 +16,9 @@ import java.util.UUID;
 public class AliOSSUtils {
 
     private String endpoint = "https://oss-cn-beijing.aliyuncs.com";
-    private String accessKeyId = "";
-    private String accessKeySecret = "";
-    private String bucketName = "";
+    private String accessKeyId = "LTAI5tQ2K8ZvruUaNaeMVTfT";
+    private String accessKeySecret = "wtzkge08eB8qfVYwzk31ciapf3JuPP";
+    private String bucketName = "car-repair-hh";
 
     /**
      * 实现上传图片到OSS
